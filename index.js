2ae8eb8ad1415f96184e7d86830bc67872528f59
chico <chi187@gmail.com>
2016-03-24 03:41:33 +0300

    LTS release

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var path = require('path');
+
+module.exports = {
+  rules: {
+    "no-js": function(context) {
+      return {
+        Program: function(node) {
+          if (path.extname(context.getFilename()) === '.js') {
+            context.report(node, 'You are attempting to use JavaScript. U MAD?');
+          }
+        }
+      };
+    },
+  },
+};
